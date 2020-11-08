c82a67e61b85fd29f8838db8a3ad4d27557e7d0d
pradyut4u <60035671+pradyut4u@users.noreply.github.com>
2020-11-08 10:49:30 +0530

    Add files via upload

---
+++ screens/LoginScreen.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
+import firebase from 'firebase'
+import db from '../config.js'
+
+export default class LoginScreen extends React.Component {
+constructor(props){
+super(props)
+this.state={
+    emailID : null,
+    password : null
+}
+}
+
+login=async(email,password)=>{
+    if(email && password){
+        try{
+            const response = await firebase.auth().signInWithEmailAndPassword(email,password)
+            if(response){
+                this.props.navigation.navigate("Transaction")
+            }
+        }
+        catch(error){
+            alert("Invalid login")
+        }
+    }
+    else{
+        alert("Enter email ID and password")
+    }
+}
+
+render(){
+	return(
+	<View>
+	<Text>LoginScreen</Text>
+	<TextInput placeholder="Enter User ID"
+        keyboardType = "email-address"
+        onChangeText = {i=>{this.setState({
+        emailID : i
+        })}}/>
+        <TextInput placeholder="Enter password"
+        secureTextEntry = {true}
+        onChangeText = {i=>{this.setState({
+        password : i
+        })}}/>
+        <TouchableOpacity onPress={()=>{this.login(this.state.emailID,this.state.password)}}>
+        <Text>Login</Text>
+        </TouchableOpacity>
+	</View>
+	)
+}
+}
---
+++ screens/SearchScreen.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler'
+import db from '../config.js'
+
+export default class Searchscreen extends React.Component {
+constructor(props){
+super(props)
+this.state={
+    allTransactions : [],
+    lastDocument : "",
+    search : ""
+}
+}
+componentDidMount=async()=>{
+    const query = await db.collection("transaction").limit(10).get()
+    query.docs.map(a=>{
+        this.setState({
+        allTransactions : [],
+        lastDocument : a
+        })
+    })
+}
+
+fetchMoreTransactions=async()=>{
+    const query = await db.collection("transaction").startAfter(this.state.lastDocument).limit(10).get()
+    query.docs.map(a=>{
+        this.setState({
+        allTransactions : [...this.state.allTransactions,a.data()],
+        lastDocument : a
+        })
+    })
+}
+searchTransaction=async(text)=>{
+    var text = text.toLowerCase()
+    var splitText = text.split("")
+    console.log("st")
+    console.log(text)
+    if(splitText[0].toLowerCase()==="b"){
+    const query = await db.collection("transaction").where("bookid","==",this.state.search).get()
+    query.docs.map(a=>{
+        this.setState({
+        allTransactions : [...this.state.allTransactions,a.data()],
+        lastDocument : a
+        })
+    })
+    }
+    if(splitText[0].toLowerCase()==="s"){
+    const query = await db.collection("transaction").where("studentid","==",this.state.search).get()
+    query.docs.map(a=>{
+        this.setState({
+        allTransactions : [...this.state.allTransactions,a.data()],
+        lastDocument : a
+        })
+    })
+    }
+}
+
+    render() {
+      return (
+      <View>
+        <TextInput placeholder="Enter Book ID / Student ID"
+        onChangeText = {i=>{this.setState({
+        search : i
+        })}}/>
+        <TouchableOpacity onPress={()=>{this.searchTransaction(this.state.search)}}>
+        <Text>Search</Text>
+        </TouchableOpacity>
+        <FlatList data={this.state.allTransactions}
+        renderItem={
+        ({item})=>(<View>
+        <Text>{"bookid:"+item.bookid}</Text>
+        <Text>{"studentid:"+item.studentid}</Text>
+        <Text>{"transactionDate:"+item.transactionDate}</Text>
+        <Text>{"transactionType:"+item.transactionType}</Text>
+        </View>)}
+        onEndReached={this.fetchMoreTransactions}
+        onEndReachedThreshhold={0.7}
+        >
+        </FlatList>
+        </View>
+      );
+    }
+  }
